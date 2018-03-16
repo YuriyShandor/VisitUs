29de53c778680cbaa0262d53433c2b4326743a15
Yuriy Shandor <yuriyshandor@gmail.com>
2018-03-16 11:43:26 +0200

    Add Header. Mod_01

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
 $(document).ready(function() {
+  // Main menu burger
   $('.toggler-icon').on('click', function() {
     $(this).toggleClass('toggler-icon_close');
     $('.nav-bar').slideToggle('0.5s');
   });
+
+  // Languages
+  $('#language_ru').addClass('active');
+
+
 });

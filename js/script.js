2daf4367d89f5a1d2f18a5e87b892d71a3b0edf6
Yuriy Shandor <yuriyshandor@gmail.com>
2018-03-17 19:52:06 +0200

    Add Improve Mod_01

--- js/script.js
+++ js/script.js
@@ -46,4 +46,36 @@ $(document).ready(function() {
       }
     ]
   });
+
+  // Smoth Scroll
+  var sections = $('.nav-sections');
+  var nav = $('.header-nav');
+  var nav_height = nav.outerHeight();
+
+  $(window).on('scroll', function () {
+    var cur_pos = $(this).scrollTop();
+
+    sections.each(function() {
+      var top = $(this).offset().top - nav_height - 100;
+      var bottom = top + $(this).outerHeight() - 350;
+
+      if (cur_pos >= top && cur_pos <= bottom) {
+        nav.find('.nav-link').removeClass('active');
+        nav.find('.nav-link[href="#'+$(this).attr('id')+'"]').addClass('active');
+      } else if (cur_pos < 300) {
+        nav.find('.nav-link').removeClass('active');
+      }
+    });
+  });
+
+  $('a').on('click', function () {
+    var $el = $(this);
+    var id = $el.attr('href');
+
+    $('html, body').animate({
+      scrollTop: $(id).offset().top - nav_height
+    }, 700);
+
+    return false;
+  });
 });
